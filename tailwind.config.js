47464b485a164818bc56c07ec191743d3ae74758
Rayhan Gumay <rayhangumay2004@gmail.com>
2025-12-14 18:52:58 +0700

    setup tailwiind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,php}",
+    "./src/**/*.{html,php}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'figtree': ['Figtree', 'system-ui', '-apple-system', 'sans-serif'],
+      },
+      colors: {
+        'sky-primary': '#0ea5e9',
+        'sky-hover': '#0284c7',
+        'light-blue': '#00B4D8',
+      }
+    },
+  },
+  plugins: [],
+}
+
