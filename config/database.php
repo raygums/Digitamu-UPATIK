b0354510d72990e7d85e250a4dcb1a3c8de6a06a
Rayhan Gumay <rayhangumay2004@gmail.com>
2026-01-01 15:12:10 +0700

    modified database

--- config/database.php
+++ config/database.php
@@ -13,7 +13,6 @@ if (!$db) {
     die("Koneksi error: " . pg_last_error()); 
 }
 
-// Fungsi helper untuk query
 function query($sql, $params = []) {
     global $db;
     if (empty($params)) {
@@ -24,25 +23,21 @@ function query($sql, $params = []) {
     return $result;
 }
 
-// Fungsi helper untuk fetch all
 function fetchAll($result) {
     if (!$result) return [];
     return pg_fetch_all($result) ?: [];
 }
 
-// Fungsi helper untuk fetch one
 function fetchOne($result) {
     if (!$result) return null;
     return pg_fetch_assoc($result);
 }
 
-// Fungsi helper untuk escape string
 function escape($string) {
     global $db;
     return pg_escape_string($db, $string);
 }
 
-// Fungsi helper untuk last insert id (PostgreSQL)
 function lastInsertId($table, $column = 'id') {
     global $db;
     $result = pg_query($db, "SELECT currval(pg_get_serial_sequence('{$table}', '{$column}'))");
